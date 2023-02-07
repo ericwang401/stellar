9d881a97a4ab01cbf77d3ab65af1e568fe37a9d6
Eric Wang <eric.wang401@gmail.com>
2023-02-07 01:18:41 +0000

    add parallax

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,21 @@ import Testimonial from 'components/landing/Testimonial'
 import LandingLayout from 'components/elements/layouts/LandingLayout'
 import { NextPageWithLayout } from 'pages/_app'
 import Gallery from 'components/landing/Gallery'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 const Landing: NextPageWithLayout = () => {
     return (
         <div>
-            <Header />
-            <Testimonial />
-            <Gallery />
+            <Parallax pages={3}>
+                <ParallaxLayer speed={0.9}>
+                    <Header />
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={0.8} speed={0.7}>
+                    <Testimonial />
+                    <Gallery />
+                </ParallaxLayer>
+            </Parallax>
         </div>
     )
 }
