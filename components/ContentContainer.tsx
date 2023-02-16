5aa8040a44b3e0fb8974495b71ea4141b0e1a225
Eric Wang <eric.wang401@gmail.com>
2023-02-16 04:43:33 +0000

    fix whitespace in contentcontainer

--- components/ContentContainer.tsx
+++ components/ContentContainer.tsx
@@ -8,7 +8,7 @@ interface Props {
 const ContentContainer = styled.div<Props>`
   ${tw`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8`}
 
-  ${({ includeYPadding }) => (includeYPadding ? tw`py-16 md:py-24 lg:py-32` : '')}
-`
+  ${({ includeYPadding }) => (includeYPadding ? tw`py-32` : '')}
+` // py-16 md:py-24 lg:py-32
 
 export default ContentContainer
